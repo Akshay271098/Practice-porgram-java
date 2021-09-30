79bebcf462c2b8cc243050727dba61e7e474a9b3
Akshay271098 <60260419+Akshay271098@users.noreply.github.com>
2021-09-30 12:29:16 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,16 @@
+//To print given array
+class Demo1
+{
+	public static void main(String args[])
+	{
+		int a[]={1,2,3,4};
+		System.out.println("length of array="+a.length);
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+			
+		}
+		int b[];
+		System.out.println(new b[]={1,2,3,4,5});
+	}
+}
